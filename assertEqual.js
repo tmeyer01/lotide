d2cb5592f56e9e18fece5c3a5bce96eb6358e6a2
thomas <tmeyerwebdev@gmail.com>
2021-09-13 21:45:25 +0000

    Created assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+const assertEqual = function(actual, expected) {
+  //console.assert(actual === expected);
+
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1,1);
+assertEqual(123,123);
+assertEqual("crust", "crust");
+assertEqual("@", "@");
+assertEqual("Campagnolo", "Shimano");
