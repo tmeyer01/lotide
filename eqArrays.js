772c18f0183cbbae69bf15b9b1a43d18505f7bf1
thomas <tmeyerwebdev@gmail.com>
2021-09-15 01:07:33 +0000

    Cleaned up code, after seeing errors via esLint

--- eqArrays.js
+++ eqArrays.js
@@ -1,5 +1,5 @@
 const assertEqual = function(actual, expected) {
-    if (actual === expected) {
+  if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual}  === ${expected} `);
   } else {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
@@ -19,8 +19,8 @@ const eqArrays = function(array1, array2) {
   return true;
 };
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3]));
-console.log(eqArrays([1, 2, 3], [3, 2, 1]));
+// console.log(eqArrays([1, 2, 3], [1, 2, 3]));
+// console.log(eqArrays([1, 2, 3], [3, 2, 1]));
 
 // console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]));
 // console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]));
