7f527b60de1ce1d9b3f60b9ed0a0be7a8d873a1a
thomas <tmeyerwebdev@gmail.com>
2021-09-15 01:05:05 +0000

    eqArrays was missing a return causing an error in both assertArrays and eqArrays (fixed)

--- eqArrays.js
+++ eqArrays.js
@@ -1,7 +1,5 @@
 const assertEqual = function(actual, expected) {
-  //console.assert(actual === expected);
-
-  if (actual === expected) {
+    if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual}  === ${expected} `);
   } else {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
@@ -17,18 +15,16 @@ const eqArrays = function(array1, array2) {
         return false;
       }
     }
-
-
   }
-
-
+  return true;
 };
+
 console.log(eqArrays([1, 2, 3], [1, 2, 3]));
 console.log(eqArrays([1, 2, 3], [3, 2, 1]));
 
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]));
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]));
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]));
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]));
 
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
