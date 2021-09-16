3d0c0a2b6e9e868efa6bbdaa7f66362b0959c6ea
thomas <tmeyerwebdev@gmail.com>
2021-09-16 02:04:03 +0000

    Initial commit

---
+++ letterPositions.js
@@ -0,0 +1,17 @@
+const letterPositions = function(sentence) {
+  let result = {};
+  
+  for (let i = 0; i < sentence.length; i++) {
+    let letter = sentence[i];
+    
+    if (!(letter in result)) {
+      result[letter] = [];
+    }
+    result[letter].push(i);
+  }
+  console.log(result);
+
+};
+
+let sentence = "lighthouse in the house";
+letterPositions(sentence);
