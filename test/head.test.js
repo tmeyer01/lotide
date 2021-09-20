51a87f31677aa49a8627736cac688de4b693a7ba
thomas <tmeyerwebdev@gmail.com>
2021-09-20 23:17:05 +0000

    refactor test to now use chai

--- test/head.test.js
+++ test/head.test.js
@@ -1,6 +1,14 @@
-const assertEqual = require('../assertEqual');
+
+const assert = require('chai').assert;
 const head = require('../head');
 
-
-assertEqual(head([7,6,7,8]), 7);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "faster");
+describe('#head', () => {
+  it('returns 1 for [1,2,3]', () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+});
+describe('#head', () => {
+  it('returns 1 for [1]', () => {
+    assert.strictEqual(head([1]), 1);
+  });
+});
