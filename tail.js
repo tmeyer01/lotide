52a48684a405b68d506fa60f99f2d6f5ddf2998a
thomas <tmeyerwebdev@gmail.com>
2021-09-20 20:26:01 +0000

    Refactoring code to export module

--- tail.js
+++ tail.js
@@ -1,36 +1,15 @@
-const assertEqual = function(actual, expected) {
-  //console.assert(actual === expected);
-
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 
 const tail = function(array) {
-  
   let tail = [];
-
-
   if (array.length === 0) {
     tail = array[0];
   } else {
     tail = array.slice(1);
   }
-
-  //console.log(tail);
-  
   return tail;
-
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
 
 
+module.exports = tail;
 
-//assertEqual(tail([7,6,7,8]), [6, 7, 8]);
-// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "faster");
