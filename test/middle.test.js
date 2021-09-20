246ce52b86e5e1aa7c295efbeee96af5b6d255d0
thomas <tmeyerwebdev@gmail.com>
2021-09-20 23:30:12 +0000

    refactor to use chai

--- test/middle.test.js
+++ test/middle.test.js
@@ -1,10 +1,5 @@
 const assert = require('chai').assert;
-const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
-
-// let x = middle([1, 2, 3, 4])
-// assertArraysEqual([1, 2, 3, 4], x);
-// assertArraysEqual([ 2, 3], [2, 3]);
 
 
 describe('#middle', () => {
--- test/tail.test.js
+++ test/tail.test.js
@@ -1,6 +1,16 @@
-const assertEqual = require('../assertEqual');
-const tail = require('../tail');
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+const assert = require('chai').assert;
+const tail = require('../tail');
+const assertEqual = require('../assertEqual');
+
+
+describe('#tail', () => {
+  it('returns 2,3 for [1,2,3]', () => {
+    assertEqual(tail([1, 2, 3]).length, 2);
+  });
+});
+describe('#tail', () => {
+  it('returns emtpy arry if only one element in arry', () => {
+    assertEqual(tail([6]).length, 0);
+  });
+});
