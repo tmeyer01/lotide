f4beb3ccbfdbe1f824ac6a7892b239f54fbea724
thomas <tmeyerwebdev@gmail.com>
2021-09-20 20:40:50 +0000

    refactor code using module export

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual}  === ${expected} `);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-
 const eqArrays = function(array1, array2) {
 
   if (array1.length === array2.length) {
@@ -18,6 +9,10 @@ const eqArrays = function(array1, array2) {
   }
   return true;
 };
+
+
+module.exports = eqArrays;
+
 
 // console.log(eqArrays([1, 2, 3], [1, 2, 3]));
 // console.log(eqArrays([1, 2, 3], [3, 2, 1]));
