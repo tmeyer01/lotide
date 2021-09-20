1e7229130eb1d268fcde8ee55cdea38daa92ba76
thomas <tmeyerwebdev@gmail.com>
2021-09-20 23:26:54 +0000

    refactor using chia

--- test/middle.test.js
+++ test/middle.test.js
@@ -1,6 +1,25 @@
+const assert = require('chai').assert;
 const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
 
-let x = middle([1, 2, 3, 4])
-assertArraysEqual([1, 2, 3, 4], x);
-assertArraysEqual([ 2, 3], [2, 3]);
+// let x = middle([1, 2, 3, 4])
+// assertArraysEqual([1, 2, 3, 4], x);
+// assertArraysEqual([ 2, 3], [2, 3]);
+
+
+describe('#middle', () => {
+  it('Return empty array for array entered with one or two elements', () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+});
+describe('#middle', () => {
+  it('returns middle element for arrays with odd number', () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+});
+describe('#middle', () => {
+  it('returns middle two elements for arrays entered with even number', () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2,3]);
+  });
+});
+
