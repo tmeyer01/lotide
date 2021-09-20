3f822e68e57b27010423e5b06e217ffec03eec70
thomas <tmeyerwebdev@gmail.com>
2021-09-20 21:09:51 +0000

    Refactoring code to use module  exports

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,19 +1,3 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length === array2.length) {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    }
-  }
-  return true;
-};
-
-
-
-
-
-
 const assertArraysEqual = function(array1, array2) {
 
   if (eqArrays(array1,array2)) {
@@ -23,6 +7,4 @@ const assertArraysEqual = function(array1, array2) {
   }
 };
 
-
-
-assertArraysEqual([1, 2, 3], [9, 2, 3]);
+module.exports = assertArraysEqual;
