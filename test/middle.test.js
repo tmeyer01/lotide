257195877063e0fc8a9dbb832feb2cd7ea34b535
thomas <tmeyerwebdev@gmail.com>
2021-09-20 21:49:40 +0000

    Inital commit for middle test

---
+++ test/middle.test.js
@@ -0,0 +1,7 @@
+const assertArraysEqual = require('../assertArraysEqual');
+const eqArrays = require('../eqArrays');
+const middle = require('../middle');
+
+let x = middle([1, 2, 3, 4])
+assertArraysEqual([1, 2, 3, 4], x);
+assertArraysEqual([ 2, 3], [2, 3]);
