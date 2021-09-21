0e5939999aff44551b8eddd67c0e71b2d9245388
thomas <tmeyerwebdev@gmail.com>
2021-09-21 04:52:11 +0000

    refactor code removed bugs

--- test/assertArraysEqual.test.js
+++ test/assertArraysEqual.test.js
@@ -1,6 +1,16 @@
+
 const assertArraysEqual = require('../assertArraysEqual');
 
 let a = [1, 2, 3];
 let b = [1, 2, 3];
 
 assertArraysEqual(a, b);
+
+// const assert = require('chai').assert;
+// const assertArraysEqual = require('../assertArraysEqual');
+
+// describe('#asserArraysEqual', () => {
+//   it('Returns true if arrays are equal [1,2,3] [1,2,3]', () => {
+//    assert.strictEqual(assertArraysEqual([1,2,3]),[1,2,3]);
+//   });
+// });
--- test/eqArrays.test.js
+++ test/eqArrays.test.js
@@ -1,10 +1,19 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const eqArrays = require('../eqArrays');
 
+describe('#eqArray', () => {
+  it('See if arrays are the same [1, 2, 3], [1, 2, 3]) returns  true', () => {
+    assert.deepEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  });
+});
+describe('#eqArray', () => {
+  it('See if the arrays are the same ["1", "2", "3"], ["1", "2", 3]), false', () => {
+    assert.deepEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+  });
+});
 
 
 
 
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
